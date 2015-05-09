73199d9df74de6ab4b4e3d8e2fad006294ff5a3d
RemiPrevost <prevost-r@hotmail.com>
2015-05-09 14:27:06 +0200

    Fonctions de créations du mot de pass temporaire, remplissage de la base de donnée et cryptage du message envoyé.

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php
+	/*Generateur de string aleatoire de taille définie par la variable locale $lenght*/
+	function password_generator() {
+		$length = 10;
+
+		$randomString = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+
+		return $randomString;
+	}
+	
+	/* Crypte le message reçu ave le chiffrement
+	 *  Rijndael-128bits et retourne le message crypté au format Base64
+	*/
+	function cryptor($data) {
+		$algo = MCRYPT_RIJNDAEL_128;
+		$key = "fh2g 0f??qg+";
+		$mode = MCRYPT_MODE_CBC;
+
+		$keyHash = md5($key);
+		$key = substr($keyHash, 0, mcrypt_get_key_size($algo, $mode));
+	    $iv  = substr($keyHash, 0, mcrypt_get_block_size($algo, $mode));
+
+	    return base64_encode(mcrypt_encrypt($algo,$key,$data,$mode,$iv));
+	}
+
+	/* Formate un message avec un mot de passe aléatoire suivi d'un salt et
+	 * de l'identifiant utilisateur
+	*/ 
+	function message_creator($name_user,$password) {
+		$salt = "£Cf1Asv( %";
+		return $password.$salt.$name_user;
+	}
 	
 	try {
 		$bdd = new PDO('mysql:host=localhost;dbname=skillshareinsa', 'root', '');
@@ -24,48 +55,23 @@
 		phpCAS::logout();
 	}
 
-	if (isset($_GET['is_connected']) && sizeof( $_GET['is_connected']) != null) {
-		$name_user = $_GET['is_connected'];
 
-		$users = $bdd->prepare('SELECT * FROM users WHERE name_user = ?');
-		$users->execute(array($name_user));
-		
-		$user = NULL;
-		$user = $users->fetch();
+	$name_user = phpCAS::getUser();
+	$password = password_generator();
 
-		if ($user == NULL) {
-			echo('0');
-		}
-		else {
-			if ($user['connected'] == 1)
-				echo('1');
-			else 
-				echo('0');
-		}
+	$exist = $bdd->prepare('SELECT COUNT(*) AS count FROM users WHERE name_user = ?');
+	$exist->execute(array($name_user));
+			
+	$exist = $exist->fetch()['count'];
+
+	if ($exist != 0) {
+		$query = $bdd->prepare('UPDATE users SET connected = 1, password = ? WHERE name_user = ?');
+		$query->execute(array($password,$name_user));
 	}
-
-
 	else {
-
-		$name_user = phpCAS::getUser();
-
-		$exist = $bdd->prepare('SELECT COUNT(*) AS count FROM users WHERE name_user = ?');
-		$exist->execute(array($name_user));
-				
-		$exist = $exist->fetch()['count'];
-
-		if ($exist != 0) {
-			$query = $bdd->prepare('UPDATE users SET connected = 1 WHERE name_user = ?');
-			$query->execute(array($name_user));
-		}
-		else {
-			$query = $bdd->prepare('INSERT INTO users (name_user, connected) VALUES (?, 1)'); 
-			$query->execute(array($name_user));
-		}
-
-		header('Location: http://localhost:3000/postConnect?user='.$name_user);
+		$query = $bdd->prepare('INSERT INTO users (name_user, password, connected) VALUES (?, ?, 1)'); 
+		$query->execute(array($name_user,$password));
 	}
 
-	
+	header('Location: http://localhost:3000/postConnect?user='.cryptor(message_creator($name_user,$password)));
 ?>
-
